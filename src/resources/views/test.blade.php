4d2e5bc3ac047cc1c38060c3e401154ce13daa14
Tiago Heineck <tiagovda@gmail.com>
2020-09-22 22:53:54 -0300

    inclui algumas rotas de teste

---
+++ src/resources/views/test.blade.php
@@ -0,0 +1,3 @@
+<x-guest-layout>    
+        <h1>Olá, isto foi um teste</h1>     
+</x-guest-layout>
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\TestController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', [TestController::class,'index']);
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
